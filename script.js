31360b02c9bb4ce8d3f103cb91f386018f1c54d6
sahasourav17 <sahasourav1170@gmail.com>
2022-12-15 20:18:25 +0600

    add functionality to display data from the api

---
+++ script.js
@@ -0,0 +1,42 @@
+let weather = {
+    "apiKey" : "fa6a6b29ae7fe7b19d02e1dcf56b5be0",
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" + city + 
+            "&units=metric&APPID=" + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const{ temp, humidity } = data.main;
+        const { speed } = data.wind;
+
+
+
+        // console.log(name,icon,description,temp,humidity,speed);
+
+        document.querySelector(".city").innerText = name;
+        document.querySelector(".temperature").innerText = `${temp}°C`;
+        document.querySelector(".icon").src = `http://openweathermap.org/img/wn/${icon}.png`
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`;
+        document.querySelector(".wind").innerText =`Wind Speed: ${speed} km/h`;
+    },
+
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+document.querySelector(".search button").addEventListener("click", function(){
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function(e){
+    if (e.key == "Enter"){
+        weather.search();
+    }
+});
